1ac8f22c2debd3b69d50cbfb6c9624ede3f94328
tanaka yoshiki <a10hhmotbssl@icloud.com>
2023-11-10 04:15:45 +0900

    ファイル作成

---
+++ src/database/seeders/PrefecturesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PrefecturesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param=[
+            'name'=>"東京"
+        ];
+        DB::table('prefectures')->insert($param);
+        $param=[
+            'name'=>"大阪"
+        ];
+        DB::table('prefectures')->insert($param);
+        $param=[
+            'name'=>"福岡"
+        ];
+        DB::table('prefectures')->insert($param);
+    }
+}
