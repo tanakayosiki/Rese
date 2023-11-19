324c802a1511ef712437d055775f6a33423cbb36
tanaka yoshiki <a10hhmotbssl@icloud.com>
2023-11-19 11:00:07 +0900

    テーブル作成

--- src/database/migrations/2023_11_16_033031_create_times_table.php
+++ src/database/migrations/2023_11_16_033031_create_times_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReservationsTable extends Migration
+class CreateTimesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateReservationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reservations', function (Blueprint $table) {
+        Schema::create('times', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('shop_id')->constrained()->cascadeOnDelete();
-            $table->date('date');
             $table->time('time');
-            $table->integer('number_of_people');
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateReservationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reservations');
+        Schema::dropIfExists('times');
     }
 }
---
+++ src/database/migrations/2023_11_16_042828_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        Schema::create('reservations', function (Blueprint $table){
+        $table->id();
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('shop_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('time_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('person_id')->constrained()->cascadeOnDelete();
+        $table->date('date');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
